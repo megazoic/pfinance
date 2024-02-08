13998d21dff6a2a1c984d9444b5b0423d8a9e282
Nic Dev <nick@nicksit.com>
2024-02-07 21:17:56 -0800

    set up for deploying postgres

--- config/sequel.rb
+++ config/sequel.rb
@@ -1,3 +1,7 @@
 require 'sequel'
 
-DB = Sequel.connect("postgres://FinanceTracker:@localhost/FinanceTracker_#{ENV.fetch('RACK_ENV', 'development')}")
+if ENV.fetch('RACK_ENV') == 'production'
+    DB = Sequel.connect("postgres://pFinance:#{ENV.fetch('PG_PWD')}@localhost/pFinance_production")
+else
+    DB = Sequel.connect("postgres://FinanceTracker:@localhost/FinanceTracker_#{ENV.fetch('RACK_ENV', 'development')}")
+end
