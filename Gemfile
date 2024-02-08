193f2af38bd0950a9ca2402cd6bfd57daa5c066f
Nic Dev <nick@nicksit.com>
2024-02-08 10:38:06 -0800

    more changes to set up prod server

--- Gemfile
+++ Gemfile
@@ -10,13 +10,10 @@ gem "pg"
 gem "sequel"
 gem 'sequel_pg', '>= 1.8', require: 'sequel'
 gem 'rake'
+gem 'thin', '>= 1.6.4'
 
 group :test do
     gem "rspec"
     gem "coderay"
     gem "rack-test"
-end
-
-group :production do
-    gem 'thin'
 end
--- config/sequel.rb
+++ config/sequel.rb
@@ -1,7 +1,7 @@
 require 'sequel'
 
 if ENV.fetch('RACK_ENV') == 'production'
-    DB = Sequel.connect("postgres://pFinance:#{ENV.fetch('PG_PWD')}@localhost/pFinance_production")
+    DB = Sequel.connect("postgres://pfinance:#{ENV.fetch('PG_PWD')}@localhost/pfinance_production")
 else
     DB = Sequel.connect("postgres://FinanceTracker:@localhost/FinanceTracker_#{ENV.fetch('RACK_ENV', 'development')}")
 end
